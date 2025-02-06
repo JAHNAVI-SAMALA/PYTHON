8ce01e1806c604b83ddab47d88ddc33e5802cb6a
JAHNAVI-SAMALA <jahnavisamala256@gmail.com>
2025-02-06 21:05:17 +0530

    inversion count
    code to count inversions to form a sorted array from the input array

---
+++ inversionCount.py
@@ -0,0 +1,33 @@
+nums = list(map(int,input().split()))
+def merge(nums,low,high):
+    if(low>=high):
+        return
+    mid = (low+high)//2
+    merge(nums,low,mid)
+    merge(nums,mid+1,high)
+    Sort(nums,low,mid,high)
+def Sort(nums,low,mid,high):
+    global count
+    i = low
+    j = mid+1
+    k =[]
+    while(i<= mid and j <= high):
+        if(nums[i] <= nums[j]):
+            k.append(nums[i])
+            i+=1
+        else:
+            count += (mid-i+1 ) 
+            k.append(nums[j])
+            j+=1
+    while(i<=mid):
+        k.append(nums[i])
+        i+=1
+    while(j<=high):
+        k.append(nums[j])
+        j+=1
+    for ind ,value in enumerate(k):
+        nums[low+ind] = value
+global count
+count = 0
+merge(nums,0,len(nums)-1)
+print(count)
